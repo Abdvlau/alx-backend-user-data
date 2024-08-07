f901613f8d7900f4d6dbdc51a03eafe626c25479
Abdvlau <abdulabdullahiumar69@gmail.com>
2024-08-07 18:02:02 +0100

    added a 403 error handler

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -33,3 +33,12 @@ def not_authorized() -> str:
     - unauthorized error
     """
     abort(401)
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
+    """GET /api/v1/forbidden
+    Return:
+    -forbidden error
+    """
+    abort(403)
