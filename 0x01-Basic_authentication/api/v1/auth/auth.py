5b6c2c22bb5ad0738d04dcc8ee6df34066ce7e2f
Abdvlau <abdulabdullahiumar69@gmail.com>
2024-08-07 21:54:31 +0100

    Created a class to manage the API authentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,26 +1,22 @@
 #!/usr/bin/env python3
 """ Module that contains a class that manages
-Api auth
+API authentication
 """
-
 
 from flask import request
 from typing import List, TypeVar
 
-
 class Auth:
-    """A class that manages Api authentication
-    """
-    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """ """
+    """A class that manages API authentication"""
 
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ Returns False. Path and excluded_paths will be used later """
         return False
 
     def authorization_header(self, request=None) -> str:
-        """ """
-
+        """ Returns None. Request will be the Flask request object """
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):  # type: ignore
-        """ """
+        """ Returns None. Request will be the Flask request object """
         return None
